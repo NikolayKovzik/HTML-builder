9242e8e1a457230e5d28a8ed06d4d6d086f28cbd
Nikolay <nikolay.kovzik@gmail.com>
2022-05-24 21:28:46 +0300

    feat: task 04-copy-directory is solved

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs/promises');
+const path = require('path');
+const mainFolderPath = path.join(__dirname, 'files');
+const copiedFolderPath = path.join(__dirname, 'files-copy');
+
+(async function checkFolderExistence(){
+  await fs.access(copiedFolderPath);
+  await fs.rm(copiedFolderPath, { recursive: true });
+  copyDir(mainFolderPath, copiedFolderPath);
+})().catch((error)=>{
+  if(error.code === 'ENOENT') {
+    copyDir(mainFolderPath, copiedFolderPath);
+  }
+});
+
+
+async function copyDir(mainPath, copiedPath) {
+  await fs.mkdir(copiedPath, { recursive: true });
+  const files = await fs.readdir(mainPath, { withFileTypes: true });
+  files.forEach((file)=>{
+    fs.copyFile(mainPath + '/' + file.name, copiedPath + '/' + file.name);
+  });
+}
+
 
