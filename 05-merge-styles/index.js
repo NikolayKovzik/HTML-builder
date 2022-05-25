996b52540135ee436b09f39b0cfb4f634930ed1d
Nikolay <nikolay.kovzik@gmail.com>
2022-05-25 09:32:22 +0300

    feat: 05-merge-styles is solved

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs/promises');
+const {createReadStream, createWriteStream} = require('fs');
+const path = require('path');
+
+const sourceDirectory = path.join(__dirname, 'styles');
+const destinationFile = path.join(__dirname, 'project-dist', 'bundle.css');
+
+(async function createBundle(stylesFolder) {
+  const styleFiles = await fs.readdir(stylesFolder, {withFileTypes: true});
+  const writeStream = createWriteStream(destinationFile);
+  styleFiles.forEach((file)=>{
+    let filePath = path.join(stylesFolder, file.name);
+    if (!file.isDirectory() && file.isFile() && path.extname(filePath) === '.css') {
+      const readStream = createReadStream(filePath, 'utf-8');
+      let result = '';
+      readStream.on('data', chunk => result += chunk);
+      readStream.on('end', () => writeStream.write(`${result}\n`));
+      readStream.on('error', err => console.log('Error: ', err.message));      
+    }   
+  });
+})(sourceDirectory);
